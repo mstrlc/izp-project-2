a93d83576b61372d2ca03ede82158caa55c95970
matyáš strelec <matyasstrelec@gmail.com>
2021-11-12 15:53:50 +0100

    pridan datovy typ pro mnozinu

--- setcal.c
+++ setcal.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /*
-Link na wiki: https://wis.fit.vutbr.cz/FIT/st/cwk.php.cs?title=Projekt2&csid=779718&id=14723
 ! Prekladat s argumenty: gcc -std=c99 -Wall -Wextra -Werror setcal.c -o setcal
+
+Link na wiki:
+https://wis.fit.vutbr.cz/FIT/st/cwk.php.cs?title=Projekt2&csid=779718&id=14723
+
 Vstupni soubor:
 1.  Definice univerza - jeden řádek začínající "U " a pokračující mezerou oddělenými prvky,
 2.  Definice množin a relací - jeden nebo více řádků začínající "S " nebo "R " a pokračující mezerou oddělenými prvky
     (řádek začínající "S" indikuje definici množiny, "R" slouží pro definici relace),
 3.  Příkazy nad množinami a relacemi - jeden nebo více řádků začínající "C " a pokračující identifikátorem příkazu.
+
+Slovnicek:
+mnozina = set
+relace = relation
+prvek = element
 */
 
-int main()
+typedef struct // Definiton of the data type used to store sets
 {
+    char *elements[31]; // Maximal length of one element is 30 characters, 31 is used to account for null character
+    int size;           // Number of elements in set
+} set_t;
+
+int main(int argc, char *argv[])
+{
+
     return 0;
 }
