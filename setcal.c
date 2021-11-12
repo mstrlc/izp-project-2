56a5487ddfce06400a1bdef84c58fdb1a1e85247
matyáš strelec <matyasstrelec@gmail.com>
2021-11-12 15:06:21 +0100

    initial comments

--- setcal.c
+++ setcal.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+Link na wiki: https://wis.fit.vutbr.cz/FIT/st/cwk.php.cs?title=Projekt2&csid=779718&id=14723
+! Prekladat s argumenty: gcc -std=c99 -Wall -Wextra -Werror setcal.c -o setcal
+Vstupni soubor:
+1.  Definice univerza - jeden řádek začínající "U " a pokračující mezerou oddělenými prvky,
+2.  Definice množin a relací - jeden nebo více řádků začínající "S " nebo "R " a pokračující mezerou oddělenými prvky
+    (řádek začínající "S" indikuje definici množiny, "R" slouží pro definici relace),
+3.  Příkazy nad množinami a relacemi - jeden nebo více řádků začínající "C " a pokračující identifikátorem příkazu.
+*/
+
 int main()
 {
     return 0;
